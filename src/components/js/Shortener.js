8effb4200df710453b1d55e9d9e8d11f38cae665
Solomon <gbolahanbalogun5@gmail.com>
2020-04-18 22:20:29 +0100

    Update position of link label

--- src/components/js/Shortener.js
+++ src/components/js/Shortener.js
@@ -7,7 +7,7 @@ const Shortener = ({ getInput, renderUrl, urlCards, errorIds }) => {
         <div className='input-container'>
             <div className='input-container-inner'>
                 <form className='shortener-form'>
-                    <label htmlFor='link'>
+                    <label htmlFor='link' style={{ position: 'relative' }}>
                         <input onChange={getInput} className={errorIds[0]} id='link' placeholder='Shorten a link here...' />
                         <span className='no-link' id={errorIds[1]}>Please add a link</span>
                         <span className='invalid-url' id={errorIds[2]}>Please enter a valid link</span>
